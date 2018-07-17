fa244068e8b97f2e61ac85fb43926d6aecee32e6
blueturtle98 <sam_barraclough@hotmail.co.uk>
2018-07-17 21:31:47 +0100

    1st

---
+++ TalkClient/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TalkClient/TalkServer/ClientListener.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace TalkServer
+{
+    /// <summary>
+    /// Listens for TCP connections on specific port
+    /// </summary>
+    class ClientListener
+    {
+        /// <summary>
+        /// Server socket
+        /// </summary>
+        TcpListener TcpL;
+
+        /// <summary>
+        /// Port to listen on
+        /// </summary>
+        int ListenPort;
+
+
+        /// <summary>
+        /// Thread used for listening for clients
+        /// </summary>
+        Thread ListenThread;
+
+        public ClientListener(int port)
+        {
+            ListenPort = port;
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                if(TcpL != null){
+                    TcpL = new TcpListener(IPAddress.Any, ListenPort);
+                    TcpL.Start();
+                }
+                else
+                {
+                    throw new InvalidOperationException("Already listening");
+                }
+                
+
+            }catch(Exception e)
+            {
+
+            }
+        }
+
+    }
+}
